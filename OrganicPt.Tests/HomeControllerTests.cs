91db6764bee15231f30e37fc3d846e173bf2b3a9
sanjay <sanjay.au@gmail.com>
2016-11-30 14:59:08 +1100

    remove packages

---
+++ OrganicPt.Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using OrganicPt;
+using OrganicPt.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TestStack.FluentMVCTesting;
+
+namespace OrganicPt.Tests
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void RendersDefaultView()
+        {
+            var repo = new Mock<IRepository<Stock>>();
+            var sut = new HomeController(repo.Object);
+            sut.WithCallTo(x => x.Index()).ShouldRenderDefaultView();            
+        }
+    }
+}
