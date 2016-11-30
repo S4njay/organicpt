55e7fef2864819ac2656559a31a63ec3390d86b1
sanjay <sanjay.au@gmail.com>
2016-11-30 16:28:30 +1100

    OrganicPt.FunctionalTest

---
+++ OrganicPt.FunctionalTest/BrowserHost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.Seleno.Configuration;
+
+namespace OrganicPt.FunctionalTest
+{
+    public static class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl; 
+
+        static BrowserHost()
+        {
+            Instance.Run("OrganicPt", 4223);
+
+            RootUrl = Instance.Application.Browser.Url;
+        }
+    }
+}
---
+++ OrganicPt.FunctionalTest/ShouldDisplayAlistOfStocks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace OrganicPt.FunctionalTest
+{
+    [TestClass]
+    public class ShouldDisplayAlistOfStocks
+    {
+        [TestMethod]
+        public void ShouldDisplayAtLeastOneRow()
+        {
+            BrowserHost.Instance.Application.Browser
+                .Navigate()
+                .GoToUrl(BrowserHost.RootUrl);
+
+            var rows = BrowserHost.Instance.Application.Browser.FindElements(By.ClassName("row"));
+
+            Assert.IsTrue(rows.Count > 0);
+        }
+    }
+}
